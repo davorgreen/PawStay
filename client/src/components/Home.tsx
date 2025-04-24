dfffb87c254302acfd3b862f1786dcd38429a21f
Davor Grinfelder <grinfelderdavor@gmail.com>
2025-04-24 14:34:38 +0200

    display property types using propertytypecard component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,30 +1,38 @@
+import { useEffect, useState } from 'react';
 import HotelCard from '../components/HotelCard.tsx';
+import axios from 'axios';
+import PropertyTypeCard from './PropertyTypeCard.tsx';
 
-const hotelList = [
-	{
-		name: 'Sea Breeze Hotel',
-		location: 'Barcelona, Spain',
-		image:
-			'https://www.veryimportantpaws.com/wp-content/uploads/2021/11/BC4A7513-1536x1024.jpg',
-		price: '$120',
-	},
-	{
-		name: 'Mountain View Lodge',
-		location: 'Zermatt, Switzerland',
-		image:
-			'https://www.mallory.co.uk/wp-content/uploads/2022/12/Joey-1.jpg',
-		price: '$150',
-	},
-	{
-		name: 'City Lights Inn',
-		location: 'New York, USA',
-		image:
-			'https://stories-editor.hilton.com/wp-content/uploads/2023/04/Canopy-by-Hilton-Boston-Downtown-Paws-in-the-Neighborhood-Program-Jamie-Mercurio-Photography-.jpg?w=1024',
-		price: '$200',
-	},
-];
+type HotelType = 'Hotel' | 'Apartments' | 'Resorts' | 'Villas';
+
+interface HotelTypeCount {
+	type: HotelType;
+	count: number;
+}
 
 export default function Home() {
+	const [hotelList, setHotelList] = useState<HotelTypeCount[]>([]);
+	const [error, setError] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			try {
+				const res = await axios.get<HotelTypeCount[]>(
+					'api/hotels/countByType'
+				);
+				console.log(res.data);
+				setHotelList(res.data);
+			} catch (err) {
+				setError(err);
+			}
+			setLoading(false);
+		};
+
+		fetchData();
+	}, []);
+
 	return (
 		<div className='h-[1200px] bg-blue-300 px-10 py-8 flex flex-col justify-center gap-8'>
 			<h1 className='text-5xl font-bold text-start text-white'>
@@ -36,7 +44,11 @@ export default function Home() {
 			<div className='flex flex-col items-center'>
 				<div className='flex gap-6 overflow-x-auto pb-4'>
 					{hotelList.map((hotel, index) => (
-						<HotelCard key={index} {...hotel} />
+						<PropertyTypeCard
+							key={index}
+							type={hotel.type}
+							count={hotel.count}
+						/>
 					))}
 				</div>
 			</div>
---
+++ client/src/components/PropertyTypeCard.tsx
@@ -0,0 +1,35 @@
+import logo from '../assets/logo.png';
+
+type PropertyTypeCardProps = {
+	type: string;
+	count: number;
+	image?: string;
+};
+
+const PropertyTypeCard: React.FC<PropertyTypeCardProps> = ({
+	type,
+	count,
+}) => {
+	return (
+		<div className='w-96 h-full bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition duration-300 ease-in-out'>
+			<img
+				src={logo}
+				alt={type}
+				className='h-48 w-full object-contain'
+			/>
+			<div className='p-4 flex flex-col items-center gap-4'>
+				<h3 className='text-3xl font-bold text-blue-500'>{type}</h3>
+				<p className='text-gray-500 text-lg font-semibold'>
+					{count} properties
+				</p>
+			</div>
+			<div className='flex items-center justify-center mb-4'>
+				<button className='bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg px-12 py-2 rounded-xl transition duration-300'>
+					View
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default PropertyTypeCard;
