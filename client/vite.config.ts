9e077aed2bd7902f3ec0ac9408303b1f60f61b93
Davor Grinfelder <grinfelderdavor@gmail.com>
2025-07-01 14:48:51 +0200

    config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,15 +2,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
 	plugins: [tailwindcss(), react()],
 	server: {
 		proxy: {
 			'/api': {
-				target: 'https://pawstay.onrender.com/api',
+				target: 'https://pawstay.onrender.com',
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
 			},
 		},
 	},
